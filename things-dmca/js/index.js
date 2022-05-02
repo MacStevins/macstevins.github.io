555769a99d79c8a84d2f8ec47100c62308c52442
MacStevins <rvching15@gmail.com>
2022-05-02 20:49:19 +0800

    Added "Things DMCA" Project Website

---
+++ things-dmca/js/index.js
@@ -0,0 +1,8 @@
+
+var scriptId = "AKfycbz_lzsemhiF6tY7icy4uKwuxA0JdqvWHxYQKZSvC2CP_CcUQ9e23p6pICtmsKW4lv0a"
+var sheetName = "Record Labels"; // Example
+
+var urlParam = {
+	sheetName
+};
+var urlTo = "https://script.google.com/macros/s/" + scriptId + "/exec?" + new URLSearchParams(urlParam);
