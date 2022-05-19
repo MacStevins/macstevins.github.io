115c7fdd66d661c25c2ccc5d75ebe24b88051069
MacStevins <rvching15@gmail.com>
2022-05-19 14:36:04 +0800

    Updated Landing Github Page

--- things-dmca/js/index.js
+++ things-dmca/js/index.js
@@ -1,8 +1,13 @@
 
-var scriptId = "AKfycbz_lzsemhiF6tY7icy4uKwuxA0JdqvWHxYQKZSvC2CP_CcUQ9e23p6pICtmsKW4lv0a"
-var sheetName = "Record Labels"; // Example
+// var scriptId = "AKfycbz_lzsemhiF6tY7icy4uKwuxA0JdqvWHxYQKZSvC2CP_CcUQ9e23p6pICtmsKW4lv0a"
+// var sheetName = "Record Labels"; // Example
 
-var urlParam = {
-	sheetName
-};
-var urlTo = "https://script.google.com/macros/s/" + scriptId + "/exec?" + new URLSearchParams(urlParam);
+// var urlParam = {
+// 	sheetName
+// };
+// var urlTo = "https://script.google.com/macros/s/" + scriptId + "/exec?" + new URLSearchParams(urlParam);
+
+function setCookie(name, value) {
+	
+}
+
