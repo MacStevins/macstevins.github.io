557bdade0328b975c9b72de840ba0ff7c60f2768
MacStevins <rvching15@gmail.com>
2024-12-25 12:40:55 +0800

    URL Bug Fix

--- js/global.js
+++ js/global.js
@@ -1,5 +1,5 @@
 if(window.location.href.lastIndexOf("index.html"))
 	window.history.replaceState({}, "", window.location.href.replace("index.html", ""))
 
-if(window.location.href.endsWith("/") && !window.location.href.endsWith(window.location.origin))
+if(window.location.href.endsWith("/") && !window.location.href.endsWith(window.location.origin + "/"))
 	window.location.href = window.location.href.slice(0, -1)
