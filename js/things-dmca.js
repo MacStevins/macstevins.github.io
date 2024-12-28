8649ff44646e9d9bd2cecf421cc56f3e714837e7
MacStevins <rvching15@gmail.com>
2024-12-29 00:13:29 +0800

    Stuffs

--- js/things-dmca.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World");
-
-// https://script.google.com/macros/s/AKfycbz_lzsemhiF6tY7icy4uKwuxA0JdqvWHxYQKZSvC2CP_CcUQ9e23p6pICtmsKW4lv0a/exec
-
-$.getJSON("test/template/cloudheadrecords.json", function(data) {
-	console.log(btoa(JSON.stringify(data)));
-});
