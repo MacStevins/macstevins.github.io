f004621baf870323e4f37e27def2f9c91352fffe
MacStevins <minifier@macstevins.github.com>
2024-12-31 01:38:10 +0000

    Automated minify of b7ed6b6ca2644bc79019066a915c9aa6d990e186

--- static/js/global.js
+++ static/js/global.js
@@ -1,5 +1,1 @@
-if(window.location.href.lastIndexOf("index.html"))
-	window.history.replaceState({}, "", window.location.href.replace("index.html", ""))
-
-if(window.location.href.endsWith("/") && !window.location.href.endsWith(window.location.origin + "/"))
-	window.location.href = window.location.href.slice(0, -1)
+if(window.location.href.lastIndexOf("index.html"))window.history.replaceState({},"",window.location.href.replace("index.html",""));if(window.location.href.endsWith("/")&&!window.location.href.endsWith(window.location.origin+"/"))window.location.href=window.location.href.slice(0,-1);
